f8b6305b92fcfd450fca00a57f0cc26d0734e17c
Muhammad Hamza Ali <81586296+mhamza-ali@users.noreply.github.com>
2021-09-19 12:42:09 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCP4WxrTUbtEHn_wfLoyCLc3SCb_oNnW5E",
+    authDomain: "popabaloon-game.firebaseapp.com",
+    projectId: "popabaloon-game",
+    storageBucket: "popabaloon-game.appspot.com",
+    messagingSenderId: "13555915279",
+    appId: "1:13555915279:web:777309bad96d5f78a8217a",
+    measurementId: "G-XQDBL6GWDW"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
